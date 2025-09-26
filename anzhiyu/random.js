1e56b47a1d0b9e5f022cdd1a26867e561e55400e
heyang <heyang070105@outlook.com>
2025-09-26 19:24:26 +0800

    Site updated: 2025-09-26 19:24:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/25/hello-world/"];function toRandomPost(){
+var posts=["2025/09/25/hello-world/","2025/09/26/步入大学：自由探索与自我成长之旅/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
